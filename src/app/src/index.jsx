7b55f94f00ab456e39f0bdbda9b82223c6a933db
Shannon Lee <shannon.lee@rubikloud.com>
2020-02-18 17:10:41 -0500

    add apollo provider, fix build script

--- src/app/src/index.jsx
+++ src/app/src/index.jsx
@@ -6,6 +6,7 @@ import App from './App';
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
+import { ApolloProvider } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
 import * as serviceWorker from './serviceWorker';
@@ -33,7 +34,10 @@ client
   })
   .then(result => console.log(result));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+    </ApolloProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
